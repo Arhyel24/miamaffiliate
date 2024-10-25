30e5d78346087573b6385ee3b4c7284d9962f62f
arhyel24 <arhyelphilip024@gmail.com>
2024-10-25 14:33:17 +0100

    update admin-dashboard.tsx

--- admin-dashboard.tsx
+++ admin-dashboard.tsx
@@ -1,4 +1,4 @@
-<html lang="en">
+/*<html lang="en">
  <head>
   <meta charset="utf-8"/>
   <meta content="width=device-width, initial-scale=1.0" name="viewport"/>
@@ -322,4 +322,4 @@
    </div>
   </div>
  </body>
-</html>
+</html>*/
