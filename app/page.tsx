db42a9cd03c36bbb4413c44313b08d6ce3084d7d
arhyel24 <arhyelphilip024@gmail.com>
2024-10-29 21:37:57 +0100

    update

---
+++ app/page.tsx
@@ -0,0 +1,166 @@
+"use client";
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { SyntheticEvent, useState } from "react";
+import { FaGoogle, FaFacebookF, FaEnvelope } from "react-icons/fa";
+
+const LoginPage = () => {
+  const [loading, setLoading] = useState(false);
+  const router = useRouter();
+  const [error, setError] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e: SyntheticEvent) => {
+    e.preventDefault();
+    setLoading(true);
+    try {
+      if (!email || !password) {
+        setError("Please fill all the fields");
+        setLoading(false);
+        return;
+      }
+
+      const emailRegex = /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/;
+      if (!emailRegex.test(email)) {
+        setError("Invalid email address");
+        setLoading(false);
+        return;
+      }
+
+      const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\W).{8,}$/;
+      if (!passwordRegex.test(password)) {
+        setError(
+          [
+            "Password must be at least 8 characters long",
+            "and contain at least one uppercase letter,",
+            "one lowercase letter, and one special character.",
+          ].join("\n")
+        );
+
+        setLoading(false);
+        return;
+      }
+
+      const res = await signIn("credentials", {
+        email: email,
+        password: password,
+        redirect: false,
+      });
+      if (res?.error) {
+        setLoading(false);
+        setError("Invalid email or password");
+      } else {
+        setError("");
+        router.replace("/");
+        // router.push("/userprofile");
+      }
+    } catch (error) {
+      setError("An unexpected error occurred");
+
+      // Clear error message after 5 seconds
+      setTimeout(() => {
+        setError("");
+      }, 5000);
+    }
+  };
+  return (
+    <div className="relative min-h-screen bg-gray-100 flex items-center justify-center">
+      <div className="absolute inset-0 bg-cover bg-center background-pattern"></div>
+      <div className="relative z-10 p-8 bg-white rounded-lg shadow-lg max-w-sm mx-auto w-full">
+        <h1 className="text-2xl text-center font-semibold mb-2">
+          Welcome back!
+        </h1>
+        <p className="mb-4 text-gray-600 text-center">
+          We're really happy to see you again!
+        </p>
+        <form onSubmit={handleSubmit} className="space-y-4">
+          <input
+            type={"email"}
+            placeholder="example@123.com"
+            name="email"
+            className="w-full px-4 py-2 border rounded-md"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type={"password"}
+            placeholder="**********"
+            name="password"
+            className="w-full px-4 py-2 border rounded-md"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+
+          {error && (
+            <div
+              className="flex items-center p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400"
+              role="alert"
+            >
+              <svg
+                className="flex-shrink-0 inline w-4 h-4 me-3"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+              >
+                <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
+              </svg>
+              <span className="sr-only">Info</span>
+              <div>
+                <span className="font-medium">Error:</span> {error}
+              </div>
+            </div>
+          )}
+          <button
+            type="submit"
+            className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 flex items-center text-center justify-center"
+          >
+            {loading ? (
+              <>
+                <svg
+                  aria-hidden="true"
+                  role="status"
+                  className="inline w-4 h-4 me-3 text-white animate-spin"
+                  viewBox="0 0 100 101"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                    fill="#E5E7EB"
+                  />
+                  <path
+                    d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                    fill="currentColor"
+                  />
+                </svg>{" "}
+                Logging In...
+              </>
+            ) : (
+              "Login"
+            )}
+          </button>
+          <p className="text-center mt-4">Or Log In With</p>
+          <div className="flex justify-around mt-4">
+            <a href="#" className="text-gray-500 hover:text-gray-700">
+              <FaGoogle size={24} />
+            </a>
+            <a href="#" className="text-gray-500 hover:text-gray-700">
+              <FaFacebookF size={24} />
+            </a>
+            <a href="#" className="text-gray-500 hover:text-gray-700">
+              <FaEnvelope size={24} />
+            </a>
+          </div>
+          <p className="mt-4 text-center text-gray-600">
+            Don't have an account?{" "}
+            <a href="/signup" className="text-blue-500 hover:underline">
+              Sign Up Here
+            </a>
+          </p>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
